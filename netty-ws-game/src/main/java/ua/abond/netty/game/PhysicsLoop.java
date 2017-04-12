7ed17bb6c13b0f8676418eecc57d9d8fe2a3137b
Anthony <zaknofein2@gmail.com>
2017-04-13 00:02:10 +0300

    Implement game and physic loops
        but you should move them to the main class
        as they belong to the pipeline executor event loop

---
+++ netty-ws-game/src/main/java/ua/abond/netty/game/PhysicsLoop.java
@@ -0,0 +1,10 @@
+package ua.abond.netty.game;
+
+public class PhysicsLoop implements Runnable {
+
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ netty-ws-game/src/main/java/ua/abond/netty/game/domain/HealthPack.java
@@ -0,0 +1,8 @@
+package ua.abond.netty.game.domain;
+
+import lombok.Builder;
+
+@Builder
+public class HealthPack {
+    private Vector2 position;
+}
