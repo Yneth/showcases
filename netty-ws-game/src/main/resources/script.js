1c766ae9e0d4094ff3bd4686a8ed3881b9cfac89
Anton Bondarenko <Anton_Bondarenko@epam.com>
2017-04-14 15:03:26 +0300

    Add double buffering

--- netty-ws-game/src/main/resources/script.js
+++ netty-ws-game/src/main/resources/script.js
@@ -2,34 +2,38 @@
     var WS_URL = "ws://localhost:8082/ws";
 
     var canvas = document.getElementById("canvas");
+    var buffer = document.createElement("canvas");
+
+    var users = [];
 
 //    canvas.width = document.body.clientWidth;
     canvas.width = 1904;
+    buffer.width = 1904;
 //    canvas.height = document.body.clientHeight;
     canvas.height = 952;
+    buffer.height = 952;
     canvasWidth = canvas.width;
     canvasHeight = canvas.height;
 
     var ctx = canvas.getContext("2d");
+    var bufferCtx = buffer.getContext("2d");
 
     var socket = new WebSocket(WS_URL);
     socket.onopen = function (e) {
         joinGame("Test")
     }
+    canvas.addEventListener('click', function (event) {
+        var x = event.pageX - canvas.offsetLeft,
+            y = event.pageY - canvas.offsetTop;
+        sendPosition(x, y);
+    }, false);
+
     socket.onmessage = function (e) {
         var data = e.data.split(":");
         var cmd = data[0];
         switch (cmd) {
             case "0": {
-                var userPositions = data[1].split(';');
-                ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-                for (var i = 0; i < userPositions.length; i++) {
-                    var userPos = userPositions[i].split(',');
-                    ctx.beginPath();
-                    ctx.arc(+userPos[0], +userPos[1], 20, 0, 2 * Math.PI);
-                    console.log(userPos[0] + "   " + userPos[1]);
-                    ctx.stroke();
-                }
+                users = data[1].split(';');
                 break;
             }
         }
@@ -46,4 +50,20 @@
     function sendPosition(x, y) {
         socket.send("0:" + x + "," + y);
     }
+
+    function draw() {
+        bufferCtx.clearRect(0, 0, canvasWidth, canvasHeight);
+        for (var i = 0; i < users.length; i++) {
+            bufferCtx.beginPath();
+            var userPos = users[i].split(',');
+            bufferCtx.arc(+userPos[0], +userPos[1], 20, 0, 2 * Math.PI);
+            bufferCtx.stroke();
+        }
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+        ctx.drawImage(buffer, 0, 0);
+
+        window.requestAnimationFrame(draw);
+    }
+
+    window.requestAnimationFrame(draw);
 })();
