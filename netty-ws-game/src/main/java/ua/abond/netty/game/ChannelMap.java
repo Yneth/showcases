d2121b6c3d58f0e9f008ea64d5fa834d55cedf1b
Anthony <zaknofein2@gmail.com>
2017-04-17 14:13:29 +0300

    Implement physics in game loop

--- netty-ws-game/src/main/java/ua/abond/netty/game/ChannelMap.java
+++ netty-ws-game/src/main/java/ua/abond/netty/game/ChannelMap.java
@@ -1,13 +1,14 @@
 package ua.abond.netty.game;
 
-import java.util.Collection;
-import java.util.concurrent.ConcurrentMap;
-
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelId;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.util.internal.chmv8.ConcurrentHashMapV8;
 import lombok.experimental.Delegate;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 
 public class ChannelMap<E> implements ChannelGroup {
     @Delegate
@@ -35,6 +36,20 @@ public class ChannelMap<E> implements ChannelGroup {
     public void remove(Channel channel) {
         channels.remove(channel);
         userData.remove(channel.id());
+    }
+
+    public void remove(ChannelId channelId) {
+        channels.remove(channels.find(channelId));
+        userData.remove(channelId);
+    }
+
+    public ChannelId find(E e) {
+        for (Map.Entry<ChannelId, E> channelIdEEntry : userData.entrySet()) {
+            if (channelIdEEntry.getValue().equals(e)) {
+                return channelIdEEntry.getKey();
+            }
+        }
+        return null;
     }
 
     public Collection<E> values() {
