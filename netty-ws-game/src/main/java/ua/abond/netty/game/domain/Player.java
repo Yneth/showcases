79ef8af67fa147f5329f22cc75c50d3f0a7b176e
Anthony <zaknofein2@gmail.com>
2017-04-17 14:10:35 +0300

    Make Player implement Collider

--- netty-ws-game/src/main/java/ua/abond/netty/game/domain/Player.java
+++ netty-ws-game/src/main/java/ua/abond/netty/game/domain/Player.java
@@ -2,13 +2,18 @@ package ua.abond.netty.game.domain;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import ua.abond.netty.game.physics.Collider;
+import ua.abond.netty.game.physics.Rect;
 import ua.abond.netty.game.physics.Vector2;
+import ua.abond.netty.game.physics.collision.QuadNode;
 
 @Data
 @Builder
-@ToString
-public class Player {
+@ToString(of = {"name", "position"})
+@EqualsAndHashCode(of = "name")
+public class Player implements Collider {
     private Vector2 position;
     private Vector2 target;
     private Vector2 rotation;
@@ -18,19 +23,33 @@ public class Player {
 
     private String name;
 
+    private BulletCollisionHandler bulletCollisionHandler;
+
     @Override
-    public boolean equals(Object o) {
-        return false;
+    public boolean collides(Collider that) {
+        if (that.getMark().equals("bullet")) {
+            return position.copy().add(that.getPosition().copy().negate()).squareMagnitude() <= 625f;
+        }
+        return position.copy().add(that.getPosition().copy().negate()).squareMagnitude() <= 1600f;
     }
 
     @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (position != null ? position.hashCode() : 0);
-        result = 31 * result + (target != null ? target.hashCode() : 0);
-        result = 31 * result + screenWidth;
-        result = 31 * result + screenHeight;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
+    public void onCollision(Collider that) {
+        if (that.getMark().equals("bullet")) {
+            Bullet bullet = (Bullet) that;
+            if (bullet.getOwner().equals(this)) {
+                return;
+            }
+            bulletCollisionHandler.onCollision(this, bullet);
+        }
+    }
+
+    @Override
+    public String getMark() {
+        return "player";
+    }
+
+    public static QuadNode<Collider> toQuadNode(Player player) {
+        return new QuadNode<>(player, Rect.from(player.getPosition(), 40, 40));
     }
 }
