368a070d57690cba542a5c34da124c6bdcd88b56
Anthony <zaknofein2@gmail.com>
2017-04-17 14:13:29 +0300

    Implement physics in game loop

--- netty-ws-game/src/main/java/ua/abond/netty/game/server/WebSocketServer.java
+++ netty-ws-game/src/main/java/ua/abond/netty/game/server/WebSocketServer.java
@@ -22,8 +22,11 @@ import ua.abond.netty.game.domain.Bullet;
 import ua.abond.netty.game.domain.Player;
 import ua.abond.netty.game.event.Message;
 import ua.abond.netty.game.event.PlayerAddedMessage;
+import ua.abond.netty.game.event.PlayerDisconnectedMessage;
 import ua.abond.netty.game.event.PlayerShootMessage;
+import ua.abond.netty.game.physics.Collider;
 import ua.abond.netty.game.physics.Vector2;
+import ua.abond.netty.game.physics.collision.QuadNode;
 import ua.abond.netty.game.physics.collision.QuadTree;
 import ua.abond.netty.game.thread.VerboseRunnable;
 
@@ -43,7 +46,7 @@ public class WebSocketServer {
     private final Random random = new SecureRandom();
 
     private final int port;
-    private QuadTree quadTree;
+    private QuadTree<Collider> quadTree = new QuadTree<>(-10, -10, 1020, 1020, 10, 1);
     private final List<Bullet> bullets;
     private final ChannelMap<Player> channelMap;
     private final ConcurrentLinkedQueue<Message> eventBus;
@@ -73,44 +76,78 @@ public class WebSocketServer {
                     PlayerShootMessage shootMessage = (PlayerShootMessage) poll;
                     Player owner = channelMap.get(shootMessage.getChannel());
                     Vector2 rotation = owner.getRotation();
-                    bullets.add(Bullet.builder()
+                    Bullet bullet = Bullet.builder()
                             .owner(owner)
                             .position(owner.getPosition().copy())
                             .direction(rotation.copy())
-                            .build()
-                    );
+                            .build();
+                    bullets.add(bullet);
+                    quadTree.add(Bullet.toQuadNode(bullet));
                 } else if (poll instanceof PlayerAddedMessage) {
                     PlayerAddedMessage playerAddedMessage = (PlayerAddedMessage) poll;
 
                     Vector2 position = randomPosition();
-//                    quadTree.add();
-                    channelMap.put(playerAddedMessage.getChannel(), Player.builder()
+                    Player player = Player.builder()
                             .name(playerAddedMessage.getName())
                             .position(position)
+                            .rotation(Vector2.ONE)
                             .target(position)
-                            .build()
-                    );
+                            .bulletCollisionHandler((p, b) -> {
+                                channelMap.remove(channelMap.find(p));
+                                quadTree.remove(Player.toQuadNode(p));
+                            })
+                            .build();
+                    quadTree.add(Player.toQuadNode(player));
+                    channelMap.put(playerAddedMessage.getChannel(), player);
+                } else if (poll instanceof PlayerDisconnectedMessage) {
+                    PlayerDisconnectedMessage msg = (PlayerDisconnectedMessage) poll;
+                    Player player = channelMap.get(msg.getChannel());
+                    if (player != null) {
+                        channelMap.remove(msg.getChannel());
+                        quadTree.remove(Player.toQuadNode(player));
+                    }
                 }
             }
 
             for (int i = 0; i < bullets.size(); i++) {
                 Bullet bullet = bullets.get(i);
+
+                QuadNode<Collider> old = Bullet.toQuadNode(bullet);
+
                 bullet.getPosition().add(bullet.getDirection().copy().multiply(speed).multiply(deltaTime));
                 float x = bullet.getPosition().getX();
                 float y = bullet.getPosition().getY();
-                if (x > 1000 || x < 0 || y > 1000 || y < 0) {
+
+                QuadNode<Collider> updated = Bullet.toQuadNode(bullet);
+                if (x >= 1000 || x <= 0 || y >= 1000 || y <= 0) {
                     bullets.remove(i);
+                    quadTree.remove(old);
                 }
+                quadTree.update(old, updated);
             }
             for (Player player : channelMap.values()) {
                 if (player.getPosition().isCloseTo(player.getTarget(), 1f)) {
                     continue;
                 }
+                QuadNode<Collider> old = Player.toQuadNode(player);
                 Vector2 direction = player.getPosition().copy().minus(player.getTarget()).normalize();
                 player.setRotation(direction.copy());
                 Vector2 velocity = direction.multiply(speed).multiply(deltaTime);
 
                 player.getPosition().add(velocity);
+                quadTree.update(old, Player.toQuadNode(player));
+            }
+
+            for (Player player : channelMap.values()) {
+                QuadNode<Collider> node = Player.toQuadNode(player);
+                List<QuadNode<Collider>> query = quadTree.query(node.getRect());
+                for (QuadNode<Collider> other : query) {
+                    Collider object = other.getElement();
+                    if (!object.equals(player) && player.collides(object)) {
+                        object.onCollision(player);
+                        player.onCollision(object);
+                    }
+                }
             }
         }), 0, 17, TimeUnit.MILLISECONDS);
 
