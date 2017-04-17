f7b08a2978da31fb0014cd6391ecdf9f8052f53e
Anthony <zaknofein2@gmail.com>
2017-04-17 14:10:55 +0300

    Change int to float in Rect

--- netty-ws-game/src/main/java/ua/abond/netty/game/physics/Rect.java
+++ netty-ws-game/src/main/java/ua/abond/netty/game/physics/Rect.java
@@ -6,14 +6,20 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class Rect {
-    private int x;
-    private int y;
+    private float x;
+    private float y;
 
-    private int width;
-    private int height;
+    private float width;
+    private float height;
 
     public boolean isInside(Rect that) {
         return this.getX() >= that.getX() && this.getX() + this.getWidth() <= (that.getX() + that.getWidth()) &&
                 this.getY() >= that.getY() && this.getY() + this.getHeight() <= (that.getY() + that.getHeight());
+    }
+
+    public static Rect from(Vector2 position, int w, int h) {
+        float subWidth = w / 2f;
+        float subHeight = h / 2f;
+        return new Rect(position.getX() - subWidth, position.getY() - subHeight, w, h);
     }
 }
